2b0104552b10b9f2060e0a1b73d69d69be840521
sunyanxin <sunyanxin@qq.com>
2020-09-07 17:02:04 +0800

    在家继续开发签到功能

---
+++ SingIn1.java
@@ -0,0 +1,10 @@
+public class Sing{
+    private int id;
+    private String name;
+    public void sing(){
+        System.out.println("在家开发其他功能");
+    }
+    public void sing(int id){
+        System.out.println("其他功能);
+    }
+}
